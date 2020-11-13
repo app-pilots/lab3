b14146da7dc312ae702db6eee766c6ca1a1d9149
lopez damian <dennyslopez2013@gmail.com>
2020-11-13 10:46:24 -0500

    init project

---
+++ app/src/main/java/com/grupo2/grupo2_ta3_iniciosesionfirebase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.grupo2.grupo2_ta3_iniciosesionfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
